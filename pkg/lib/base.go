37f47f1d1a949e9076109e9a39ca169f8a126802
Arizona Hanson <arizona@starlig.ht>
2022-04-03 19:27:52 -0400

    comparisons

--- pkg/lib/base.go
+++ pkg/lib/base.go
@@ -14,13 +14,16 @@ var BaseLib = map[string]eval.FuncType{
 	"or":     _or,
 	"==":     _equalQ,
 	"equal?": _equalQ,
+	"!=":     _nequalQ,
+	"<":      _ltQ,
+	"lt?":    _ltQ,
+	"<=":     _lteqQ,
+	"lteq?":  _lteqQ,
+	">":      _gtQ,
+	"gt?":    _gtQ,
+	">=":     _gteqQ,
+	"gteq?":  _gteqQ,
 	/*
-		==
-		!=
-		<
-		<=
-		>
-		>=
 		+
 		-
 		*
@@ -51,6 +54,79 @@ func minLen(exp ast.Expr, n int) error {
 	return nil
 }
 
+func evalNumber(exp ast.Any, env *eval.Env) (*ast.Number, error) {
+	val, err := eval.Eval(exp, env)
+	if err != nil {
+		return nil, err
+	}
+	switch num := val.(type) {
+	default:
+		return nil, fmt.Errorf("called with non-number %#v", val)
+	case ast.Number:
+		return &num, nil
+	}
+}
+
+func _ltQ(exp ast.Expr, env *eval.Env) (ast.Any, error) {
+	if err := exactLen(exp, 3); err != nil {
+		return ast.Null{}, err
+	}
+	val1, err := evalNumber(exp[1], env)
+	if err != nil {
+		return ast.Null{}, err
+	}
+	val2, err := evalNumber(exp[2], env)
+	if err != nil {
+		return ast.Null{}, err
+	}
+	return ast.Boolean(val1.Decimal().LessThan(val2.Decimal())), nil
+}
+
+func _lteqQ(exp ast.Expr, env *eval.Env) (ast.Any, error) {
+	if err := exactLen(exp, 3); err != nil {
+		return ast.Null{}, err
+	}
+	val1, err := evalNumber(exp[1], env)
+	if err != nil {
+		return ast.Null{}, err
+	}
+	val2, err := evalNumber(exp[2], env)
+	if err != nil {
+		return ast.Null{}, err
+	}
+	return ast.Boolean(val1.Decimal().LessThanOrEqual(val2.Decimal())), nil
+}
+
+func _gtQ(exp ast.Expr, env *eval.Env) (ast.Any, error) {
+	if err := exactLen(exp, 3); err != nil {
+		return ast.Null{}, err
+	}
+	val1, err := evalNumber(exp[1], env)
+	if err != nil {
+		return ast.Null{}, err
+	}
+	val2, err := evalNumber(exp[2], env)
+	if err != nil {
+		return ast.Null{}, err
+	}
+	return ast.Boolean(val1.Decimal().GreaterThan(val2.Decimal())), nil
+}
+
+func _gteqQ(exp ast.Expr, env *eval.Env) (ast.Any, error) {
+	if err := exactLen(exp, 3); err != nil {
+		return ast.Null{}, err
+	}
+	val1, err := evalNumber(exp[1], env)
+	if err != nil {
+		return ast.Null{}, err
+	}
+	val2, err := evalNumber(exp[2], env)
+	if err != nil {
+		return ast.Null{}, err
+	}
+	return ast.Boolean(val1.Decimal().GreaterThanOrEqual(val2.Decimal())), nil
+}
+
 func _equalQ(exp ast.Expr, env *eval.Env) (ast.Any, error) {
 	if err := minLen(exp, 3); err != nil {
 		return ast.Null{}, err
@@ -69,6 +145,21 @@ func _equalQ(exp ast.Expr, env *eval.Env) (ast.Any, error) {
 		}
 	}
 	return ast.Boolean(true), nil
+}
+
+func _nequalQ(exp ast.Expr, env *eval.Env) (ast.Any, error) {
+	if err := exactLen(exp, 3); err != nil {
+		return ast.Null{}, err
+	}
+	val1, err := eval.Eval(exp[1], env)
+	if err != nil {
+		return ast.Null{}, err
+	}
+	val2, err := eval.Eval(exp[2], env)
+	if err != nil {
+		return ast.Null{}, err
+	}
+	return ast.Boolean(!val1.Equal(val2)), nil
 }
 
 func _and(exp ast.Expr, env *eval.Env) (ast.Any, error) {
